92c318fa159923de28b1b054a529da73ea5412ad
Soleng <sola-prog@live.fr>
2023-12-01 18:29:41 +0100

    Day 1 : part 2

--- d1/calibrationValueFinder.py
+++ d1/calibrationValueFinder.py
@@ -1,18 +1,42 @@
 import re
 
+rPattern = r'''\s*
+(?(DEFINE)
+  (?<one_to_9>
+  (?:f(?:ive|our)|s(?:even|ix)|t(?:hree|wo)|(?:ni|o)ne|eight)
+  )
+  (?<zero_to_9>
+  (?&one_to_9)|zero
+  )
+)
+((?&zero_to_9)|\d).*(\d|(?&zero_to_9))|(\d|(?&zero_to_9))[a-zA-Z]*
+'''
+
+numberSpelled = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+
+def converToNumber(match):
+    for idx, word in enumerate(numberSpelled):
+        if match == word:
+            return str(idx)
+    return match
+
+
 def calibrationValueFinder():
     value = 0
     file = open('input.txt', "r+")
     for line in file:
-        matches = re.search(r"(\d).*(\d)|(\d)[a-zA-Z]*", line)
+        matches = re.search(
+            r'((?:f(?:ive|our)|s(?:even|ix)|t(?:hree|wo)|(?:ni|o)ne|eight)|zero|\d).*(\d|(?:f(?:ive|our)|s(?:even|ix)|t(?:hree|wo)|(?:ni|o)ne|eight)|zero)|(\d|(?:f(?:ive|our)|s(?:even|ix)|t(?:hree|wo)|(?:ni|o)ne|eight)|zero)[a-zA-Z]*',
+            line)
         if not matches:
             continue
 
         if matches[2]:
-            value += int(matches[1] + matches[2])
+            value += int(converToNumber(matches[1]) + converToNumber(matches[2]))
             print("2 values : " + matches[1] + matches[2])
         else:
-            value += int(matches[3] + matches[3])
+            value += int(converToNumber(matches[3]) + converToNumber(matches[3]))
             print("1 values : " + matches[3] + matches[3])
 
     print(value)
