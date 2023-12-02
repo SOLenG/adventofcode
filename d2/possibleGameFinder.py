46496b60bcf3f8e2b93f21e0fa40333ff3a22a93
Soleng <sola-prog@live.fr>
2023-12-02 18:51:22 +0100

    Day 2 : part 2

--- d2/possibleGameFinder.py
+++ d2/possibleGameFinder.py
@@ -2,12 +2,6 @@ import re
 
 
 def possible_game_finder():
-    # color for alert
-    red_alert = '\033[91m'
-    end_alert = '\033[0m'
-    green_alert = '\033[92m'
-    # limit target we shouldn't exceed
-    target = {'red': 12, 'green': 13, 'blue': 14}
     # result
     value = 0
 
@@ -18,34 +12,21 @@ def possible_game_finder():
             print('\033[91m Matches is empty \033[0m')
             continue
 
-        failed = False
+        current_value = {'red': 0, 'green': 0, 'blue': 0}
         game_set = matches[2].split(';')
         for gSet in game_set:
             details_matches = re.findall(r'(\d+)\s(\w+)', gSet)
-            current_value = {'red': 0, 'green': 0, 'blue': 0}
             if not details_matches:
                 continue
 
             for d in details_matches:
-                current_value[d[1]] += int(d[0])
-                if current_value[d[1]] > target[d[1]]:
-                    failed = True
-                    break
-            else:
-                continue
-            break
+                if current_value[d[1]] < int(d[0]):
+                    current_value[d[1]] = int(d[0])
 
-        if not failed:
-            value += int(matches[1])
-            print('game : ' + matches[1] + green_alert + ' - succeed' + end_alert)
-        else:
-            print('games : ' + matches[1] + ' - red : ' + end_alert +
-                  (green_alert, red_alert)[current_value['red'] > target['red']]
-                  + str(current_value['red']) + '/' + str(target['red']) + end_alert + ' - green : ' + end_alert +
-                  (green_alert, red_alert)[current_value['green'] > target['green']]
-                  + str(current_value['green']) + '/' + str(target['green']) + end_alert + ' - blue : ' + end_alert +
-                  (green_alert, red_alert)[current_value['blue'] > target['blue']]
-                  + str(current_value['blue']) + '/' + str(target['blue']) + end_alert)
+        value += current_value['red'] * current_value['green'] * current_value['blue']
+        print('games : ' + matches[1] + ' - red : ' + str(current_value['red'])
+              + ' - green : ' + str(current_value['green'])
+              + ' - blue : ' + str(current_value['blue']))
 
     print(value)
 
