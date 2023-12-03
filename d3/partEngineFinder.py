69c75dafb698d30d9061b4249ec143c0842e4332
Soleng <sola-prog@live.fr>
2023-12-03 18:46:50 +0100

    Day 3 : part 2

--- d3/partEngineFinder.py
+++ d3/partEngineFinder.py
@@ -1,14 +1,21 @@
 import re
+from functools import reduce
 
 
 def part_engine_finder():
     # result
     value = 0
     # init data's memories
-    mem_line = {'current': {'number': {}, 'symbol': {}}, 'previous': {'number': {}, 'symbol': {}}}
+    mem_line = {
+        'current': {'number': {}, 'symbol': {}},
+        'previous': {'number': {}, 'symbol': {}},
+        'older': {'number': {}, 'symbol': {}}
+    }
 
     file = open('input.txt', "r+")
     for line in file:
+        # save previous data
+        mem_line['older'] = mem_line['previous']
         # save previous data
         mem_line['previous'] = mem_line['current']
         # reinit current data
@@ -19,31 +26,40 @@ def part_engine_finder():
             mem_line['current']['number'][m.start()] = {'value': m.group(), 'span': m.span()}
 
         # find symbol position and data
-        pos_symbol = re.compile('[^0-9.\n ]')
+        pos_symbol = re.compile('[*]')
         for m in pos_symbol.finditer(line):
             mem_line['current']['symbol'][m.start()] = {'value': m.group(), 'span': m.span()}
 
-        # compare current symbol
-        for ps in mem_line['current']['symbol']:
-            cs = mem_line['current']['symbol'][ps]
-            value += value_finder(cs, mem_line['current']['number'])
-            value += value_finder(cs, mem_line['previous']['number'])
-
-        # compare previous symbol
+        # find value by symbol
         for ps in mem_line['previous']['symbol']:
             cs = mem_line['previous']['symbol'][ps]
-            value += value_finder(cs, mem_line['current']['number'])
+            value += value_finder(cs, mem_line['current']['number'], mem_line['previous']['number'],
+                                  mem_line['older']['number'])
+
     print(value)
 
 
-def value_finder(cs, pns):
+def value_finder(cs, cns, pns, ons):
+    possibility = []
     value = 0
-    for pn in pns:
-        cn = pns[pn]
-        if int(cs['span'][0]) >= int(cn['span'][0]) - 1 and int(cs['span'][1]) <= int(cn['span'][1]) + 1:
-            value += int(cn['value'])
+    possibility += possibility_finder(cs, pns)
+    possibility += possibility_finder(cs, cns)
+    possibility += possibility_finder(cs, ons)
+
+    if len(possibility) > 1:
+        value += reduce((lambda x, y: x * y), possibility)
 
     return value
+
+
+def possibility_finder(cs, ns):
+    possibility = []
+    for pn in ns:
+        cn = ns[pn]
+        if int(cs['span'][0]) >= int(cn['span'][0]) - 1 and int(cs['span'][1]) <= int(cn['span'][1]) + 1:
+            possibility.append(int(cn['value']))
+
+    return possibility
 
 
 if __name__ == '__main__':
